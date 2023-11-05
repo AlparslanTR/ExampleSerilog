bb796899d03f2d3392aad8cfca4144968aaca5ac
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-11-05 19:40:30 +0300

    Proje dosyası ekle.

---
+++ ExampleSerilog/Logging/SerilogConfigure.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Core;
+
+namespace ExampleSerilog.Logging
+{
+    public class SerilogConfigure
+    {
+        // Log Ayarları dinamik olarak burada yapılmalıdır.
+        public static Logger ConfigureLogger()
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File("Log Kayıtları/HavaTahminLog-.txt", rollingInterval: RollingInterval.Minute)
+                .CreateLogger();
+        }
+    }
+}
